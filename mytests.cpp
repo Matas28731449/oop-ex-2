3dfbb2f2196d19e74ff48576c445a26df6292efc
Matas28731449 <mataslekavi@gmail.com>
2023-05-18 20:12:05 +0300

    Updated Unit tests

--- mytests.cpp
+++ mytests.cpp
@@ -14,6 +14,22 @@ TEST(student, GradeCountCheck) {
     tmp.setGrade(5);
     EXPECT_TRUE(tmp.getGrade().size() == value);
 }
+TEST(student, StudentCopyCheck) {
+    student a;
+    a.setGrade(6);
+    a.setGrade(5);
+    a.setGrade(7);
+    student b = a;
+    EXPECT_TRUE(a.getGrade().size() == b.getGrade().size());
+}
+TEST(student, StudentMoveCheck) {
+    student a;
+    a.setGrade(6);
+    a.setGrade(5);
+    a.setGrade(7);
+    student b(std::move(a));
+    EXPECT_FALSE(b.getGrade().size() == a.getGrade().size());
+}
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
 
